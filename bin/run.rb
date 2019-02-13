122d6b6b9976cdef6e0aecc4d49ece105f39fc7b
dren39 <dren391@gmail.com>
2019-02-13 18:14:13 -0500

    fixed #show_playlist, added exit ruby

--- bin/run.rb
+++ bin/run.rb
@@ -1,29 +1,54 @@
 require_relative '../config/environment'
 
+# playing = true
+# while playing = true
+#
+# end
+
+playing = true
+
 
 game = Rubyoke.new
 
 game.greeting
 
-game.main_menu
+while playing == true
 
-menu_selection = gets.chomp
+  game.main_menu
 
-game.create_playlist
+  menu_selection = gets.chomp.to_i
 
+  game.create_playlist
 
-if menu_selection == "1"
-  game.sing_along
+  if menu_selection == 1
+    game.sing_along
+    menu_selection = gets.chomp.to_i
+    if menu_selection == 1
+      game.sing_along
+    else
+      game.main_menu
+    end
+  elsif menu_selection == 2
+    game.show_playlist
+    puts "ENTER 5 FOR MAIN MENU"
+    puts "ENTER 6 TO DELETE SONG FROM PLAYLIST"
+
+    menu_selection = gets.chomp.to_i
+      if menu_selection == 6
+        puts "Enter name of the song you want to delete"
+        song_name = gets.chomp
+        # binding.pry
+        game.delete_song(song_name)
+        game.show_playlist
+      end
+
+  elsif menu_selection == 5
+    game.main_menu
+    menu_selection = gets.chomp.to_i
+  elsif menu_selection == 3
+    puts "                            GOODBYE!"
+    exit!
+  end
+
 end
-
-
-
-# sleep(60)
-#
-# game.stop_playing_song
-
-banana = game.save_to_playlist?
-
-game.to_save_or_not_to_save(banana)
-
-binding.pry
+# binding.pry
--- lib/rubyoke.rb
+++ lib/rubyoke.rb
@@ -3,12 +3,15 @@ class Rubyoke
 
   attr_accessor :song, :artist, :lyrics, :user_name
 
+#Greets the user#
   def greeting
     puts "                        WELCOME TO RUBYOKE!"
     puts "                        WHAT'S YOUR NAME?"
-    self.user_name = gets.chomp.strip
+    self.user_name = gets.chomp.strip.downcase
+    system "clear"
   end
 
+#Display the main menu#
   def main_menu
     puts "            HEY #{self.user_name.upcase}! WHAT WOULD YOU LIKE TO DO ?  (pick a number and hit ENTER) "
     puts "                        1 - SING ALONG                       "
@@ -16,36 +19,44 @@ class Rubyoke
     puts "                        3 - LEAVE RUBYOKE                    "
   end
 
+#Create a playlist for current user#
   def create_playlist
-    playlist_name = "#{self.user_name}'s playlist"
-    Playlist.create(name: playlist_name)
+    playlist_name = "#{self.user_name.downcase}'s playlist"
+    Playlist.find_or_create_by(name: playlist_name)
+    system "clear"
   end
 
+#Sing_Along method: Asks for artist and song name#
   def artist_song?
-    puts "Who's the artist?"
+    puts "                    Who's the artist?".upcase
     self.artist = gets.chomp.strip
-    puts "Which song would like to hear?"
+    puts "                Which song would you like to hear?".upcase
     self.song = gets.chomp.strip
   end
 
+#Sing_Along method: creates song in Song table#
   def create_song
     Song.find_or_create_by(name: self.song, artist: self.artist)
   end
 
-
+#Sing_Along method: store lyrics string in Song table#
   def store_lyrics
     lyrics = LyricsAPI.new(self.song, self.artist).get_lyrics
     Song.update(Song.find_by(name: self.song).id, lyrics: lyrics)
   end
 
+#Displays lyrics string#
   def display_lyrics
     lyrics = Song.find_by(name: self.song).lyrics
+    system "clear"
     puts lyrics
   end
 
+#Sing_Along method: returns true or false for to_save_or_not_to_save method#
   def save_to_playlist?
-    puts "Would you like to add this song your playlist?"
-    puts "yes/no"
+    system "clear"
+    puts "                Would you like to add this song your playlist?"
+    puts "                                       yes/no"
     response = gets.chomp.downcase
     if response == "yes"
       true
@@ -56,6 +67,7 @@ class Rubyoke
     end
   end
 
+#Runs method to save song or not save song#
   def to_save_or_not_to_save(answer)
     if answer == true
        self.to_save
@@ -73,7 +85,11 @@ class Rubyoke
     pid = fork{exec 'killall afplay'}
   end
 
+### Menu Methods ###
   def sing_along
+
+    play = nil
+
     self.artist_song?
 
     self.create_song
@@ -82,20 +98,56 @@ class Rubyoke
 
     self.display_lyrics
 
-    self.play_song
+    puts "Hit X to stop playing song"
 
+    self.play_song
+    play = gets.chomp.downcase
+    if play == "x"
+      self.stop_playing_song
+    end
+    self.to_save_or_not_to_save(self.save_to_playlist?)
   end
 
+  # def get_my_song_ids
+  #   Tracklist.all.map{|track| track.song_id}
+  # end
+
+  def get_my_songs_ids
+    Tracklist.all.where("playlist_id = #{self.get_user_playlist_id}").map do |track| track.song_id end.uniq
+  end
+
+  def get_user_playlist_id
+    Playlist.all.find_by(name: "#{self.user_name.downcase}'s playlist").id
+  end
+
+  def show_playlist
+    Song.all.where(id: self.get_my_songs_ids).map do |song|
+        puts "#{song.name},".capitalize + " by " + "#{song.artist}".capitalize end
+  end
+
+  #Playlist method#
+  def delete_song(song_name)
+    # binding.pry
+    Tracklist.delete(Tracklist.all.find_by(song_id: gets_song_id_by_name(song_name).id))
+  end
+
+  def gets_song_id_by_name(name)
+    Song.all.find_by(name: name).id
+  end
 ###Helper Methods###
   def to_save
       playlist_id = Playlist.find_by(name: "#{self.user_name}'s playlist").id
       song_id = Song.find_by(name: "#{self.song}").id
       # binding.pry
       Tracklist.create(playlist_id: playlist_id, song_id: song_id)
+      system "clear"
   end
 
   def not_to_save
-    puts "Ok, pick a new song"
+    system "clear"
+    puts "                Ok, enter 1 to pick a new song".upcase
+    # puts "              OR"
+    # puts "    ENTER 5 FOR MAIN MENU"
   end
 
 
