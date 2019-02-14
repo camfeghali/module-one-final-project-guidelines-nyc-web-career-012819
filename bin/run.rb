012d2a59743de03352eba5270a2c2bf489caaf52
camfeghali <camfeg@gmail.com>
2019-02-14 18:40:18 -0500

    fixed bug

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,53 @@
-# require_relative '../config/environment'
+require_relative '../config/environment'
 
-require_relative "../lib/play.rb"
 
-Rubyoke.new
+
+
+system "clear"
+
+puts_rubyoke
+
+playing = true
+
+
+game = Rubyoke.new
+
+game.greeting
+
+while playing == true
+
+  game.main_menu
+
+  menu_selection = gets.chomp.to_i
+
+  game.create_playlist
+
+  if menu_selection == 1
+    game.sing_along
+    menu_selection = gets.chomp.to_i
+    if menu_selection == 1
+      game.sing_along
+    else
+      game.main_menu
+    end
+  elsif menu_selection == 2
+    game.show_playlist
+    puts "ENTER M FOR MAIN MENU OR D TO DELETE A SONG FROM PLAYLIST".rjust(270)
+    menu_selection = gets.chomp.downcase
+      if menu_selection == "d"
+        puts "Enter name of the song you want to delete".rjust(261).red
+        song_name = gets.chomp.downcase
+        # binding.pry
+        game.delete_song(song_name)
+        game.show_playlist
+      end
+  elsif menu_selection == "m"
+    game.main_menu
+    menu_selection = gets.chomp.to_i
+  elsif menu_selection == 3
+    system "clear"
+    puts_goodbye
+    exit!
+  end
+
+end
